1217838987ab0605ec74c126fff0f3184e2b981c
Carson H <caho2973@colorado.edu>
2023-09-09 22:20:12 -0600

    Initial Commit

---
+++ StatesMobileDev/StatesMobileDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatesMobileDevApp.swift
+//  StatesMobileDev
+//
+//  Created by CUBS Customer on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct StatesMobileDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
